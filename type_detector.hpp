9c35d2b4a8c7e957762378c7ea5a478121132133
Hussam Eldin Wael <85431537+hussameldin171@users.noreply.github.com>
2023-12-23 13:53:22 +0200

    -Added is_pointer , is_integral, and is_floating_point metafunctions.

--- type_detector.hpp
+++ type_detector.hpp
@@ -41,6 +41,81 @@ namespace traits
     template<typename T , typename Void = void>
     static constexpr bool is_signed_v = is_signed<T,Void>::value;
 
+    /******************************************************************************
+	 * Metafunction: is_pointer
+	 *-----------------------------------------------------------------------------
+	 * Description:
+	 *   -This metafunction returns true if a given type is a pointer.
+     * 
+	 * Template Parameters:
+	 *   -T : The type to check whether it's a pointer type or not.
+	 *
+	 *****************************************************************************/
+    
+    template<typename T>
+    struct is_pointer_helper : false_type{};
+    
+    template<typename T>
+    struct is_pointer_helper<T *> : true_type{};
+
+    template<typename T>
+    struct is_pointer : is_pointer_helper<remove_cv_t<T>>{};
+
+    template<typename T>
+    static constexpr bool is_pointer_v = is_pointer<T>::value;
+
+    /******************************************************************************
+	 * Metafunction: is_integral
+	 *-----------------------------------------------------------------------------
+	 * Description:
+	 *   -This metafunction returns true if a given type is integral.
+     * 
+	 * Template Parameters:
+	 *   -T : The type to check whether it's integral or not.
+	 *
+	 *****************************************************************************/
+    template<typename T>
+    struct is_integral: is_in_pack<remove_cv_t<T>,
+                                bool,
+                                char,
+                                char16_t,
+                                char32_t,
+                                wchar_t,
+                                unsigned char,
+                                short,
+                                unsigned short,
+                                int,
+                                unsigned int,
+                                long,
+                                unsigned long,
+                                long long,
+                                unsigned long long>{};
+
+
+
+    template<typename T>
+    static constexpr bool is_integral_v = is_integral<T>::value;
+
+    /******************************************************************************
+	 * Metafunction: is_floating_point
+	 *-----------------------------------------------------------------------------
+	 * Description:
+	 *   -This metafunction returns true if a given type is float or double.
+     * 
+	 * Template Parameters:
+	 *   -T : The type to check whether it's floating point or not.
+	 *
+	 *****************************************************************************/
+    template<typename T>
+    struct is_floating_point : is_in_pack< remove_cv_t<T>,
+                                           float,
+                                           double,
+                                           long double
+                                           >{};
+    
+    template<typename T>
+    static constexpr bool is_floating_point_v = is_floating_point<T>::value;
+
 }
 
 #endif // __TYPE_DETECTOR_H__

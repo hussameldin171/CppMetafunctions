4d72903e0df3dc090a61f7dc584dd2bde02c74ed
Hussam Eldin Wael <85431537+hussameldin171@users.noreply.github.com>
2023-12-27 23:25:05 +0200

    -Updated constructor checking metafunctions.

--- class_properties.hpp
+++ class_properties.hpp
@@ -15,7 +15,33 @@
 #include "logic.hpp"
 namespace traits
 {
+  
     /******************************************************************************
+	 * Metafunction: is_constructible
+	 *-----------------------------------------------------------------------------
+	 * Description:
+	 *   -This metafunction checks if a type is generally constructible.
+	 * Template Parameters:
+	 *   -T : The type to check whether it is constructible or not.
+   *   -Args : The arguments to pass to the constructor.
+	 *
+	 *****************************************************************************/
+    template<typename , typename = void , typename ...>
+    struct is_constructible_helper : false_type{};
+
+    template<typename T , typename ...Args>
+    using is_constructible_property = decltype(T{std::declval<Args>()...});
+
+    template<typename T , typename ...Args>
+    struct is_constructible_helper<T , void_t< is_constructible_property<T,Args...>>, Args...> : true_type{};
+
+    template<typename T , typename ...Args>
+    struct is_constructible : is_constructible_helper<T,void,Args...>{};
+    
+    template<typename T , typename ...Args>
+    static constexpr bool is_constructible_v = is_constructible<T,Args...>::value;
+
+  /******************************************************************************
 	 * Metafunction: is_default_constructible
 	 *-----------------------------------------------------------------------------
 	 * Description:
@@ -24,38 +50,42 @@ namespace traits
 	 *   -T : The type to check whether it is default constructible or not.
 	 *
 	 *****************************************************************************/
-    template<typename T, typename = void>
-    struct is_default_constructible : false_type{};
+    template<typename T>
+    struct is_default_constructible : is_constructible<T>{};
 
     template<typename T>
-    using default_constructor_property = decltype(T{});   
+    static constexpr bool is_default_constructible_v = is_default_constructible<T>::value;
 
-    template<typename T>
-    struct is_default_constructible<T, void_t<default_constructor_property<T>>> : true_type{};//is_same<default_constructor_property<T> , T>{};
-
-    template<typename T , typename Void = void>
-    static constexpr bool is_default_constructible_v = is_default_constructible<T,Void>::value;
-
-    /******************************************************************************
-	 * Metafunction: is_constructible
+   /******************************************************************************
+	 * Metafunction: is_copy_constructible
 	 *-----------------------------------------------------------------------------
 	 * Description:
-	 *   -This metafunction checks if a type is generally constructible.
+	 *   -This metafunction checks if a type is copy constructible.
 	 * Template Parameters:
-	 *   -T : The type to check whether it is constructible or not.
+	 *   -T : The type to check whether it is copy constructible or not.
 	 *
 	 *****************************************************************************/
-    template<typename , typename = void , typename ...>
-    struct is_constructible : false_type{};
+    template<typename T>
+    struct is_copy_constructible : is_constructible<T , const T&>{};
 
-    template<typename T , typename ...Args>
-    using is_constructible_property = decltype(T{std::declval<Args>()...});
+    template<typename T>
+    static constexpr bool is_copy_constructible_v = is_copy_constructible<T>::value;
 
-    template<typename T , typename ...Args>
-    struct is_constructible<T , void_t< is_constructible_property<T,Args...>>, Args...> : true_type{};
+   /******************************************************************************
+	 * Metafunction: is_move_constructible
+	 *-----------------------------------------------------------------------------
+	 * Description:
+	 *   -This metafunction checks if a type is move constructible.
+	 * Template Parameters:
+	 *   -T : The type to check whether it is move constructible or not.
+	 *
+	 *****************************************************************************/
+    template<typename T>
+    struct is_move_constructible : is_constructible<T , T&&>{};
 
-    template<typename T , typename Void = void , typename ...Args>
-    static constexpr bool is_constructible_v = is_constructible<T,Void,Args...>::value;
+    template<typename T>
+    static constexpr bool is_move_constructible_v = is_move_constructible<T>::value;
+
 
 
 }
